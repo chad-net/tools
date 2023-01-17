fb37d8814d4b617e7cbcdbd60e35577cbbe23cf2
clusk <clusk@strike.me>
2023-01-17 13:06:47 -0600

    swag

--- update.sh
+++ update.sh
@@ -2,4 +2,7 @@
 shpath="$(dirname "$(readlink -f "$0")")"
 perl -i -pe "s/<p>Total links: <strong>.*$/<p>Total links: <strong>$(cat "$shpath/CHADNET_LINK_COUNT")<\/strong>. Last updated on $(date --utc +'%B %d, %Y, at %H:%M UTC').<\/p>/" "$shpath/../index.html"
 rm "$shpath/CHADNET_LINK_COUNT"
-find $shpath/../ -type d \( -path $shpath \) -prune -o -exec chmod 644 {} \;
+chmod 644 $shpath/../*
+chmod 755 $shpath/../tools
+chmod 755 $shpath/../files
+chmod 644 $shpath/../files/*
