be4d286770b133c73c276e5a3cace2660e61daa3
clusk <clusk@strike.me>
2023-01-17 13:05:32 -0600

    add line to update.sh

--- chmod.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-shpath="$(dirname "$(readlink -f "$0")")"
-find $shpath/../ -type d \( -path $shpath \) -prune -o -exec chmod 644 {} \;
-
--- update.sh
+++ update.sh
@@ -2,3 +2,4 @@
 shpath="$(dirname "$(readlink -f "$0")")"
 perl -i -pe "s/<p>Total links: <strong>.*$/<p>Total links: <strong>$(cat "$shpath/CHADNET_LINK_COUNT")<\/strong>. Last updated on $(date --utc +'%B %d, %Y, at %H:%M UTC').<\/p>/" "$shpath/../index.html"
 rm "$shpath/CHADNET_LINK_COUNT"
+find $shpath/../ -type d \( -path $shpath \) -prune -o -exec chmod 644 {} \;
