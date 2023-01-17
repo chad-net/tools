a49d01feb7444899575bdc056680ff492223df4c
clusk <clusk@strike.me>
2023-01-17 12:36:00 -0600

    add chmod script

---
+++ chmod.py
@@ -0,0 +1,8 @@
+import os  
+
+path = "../*"  
+for root, dirs, files in os.walk(path):  
+  for n in dirs:  
+    os.chown(os.path.join(root, n), 644, 20)
+  for n in files:
+    os.chown(os.path.join(root, n), 644, 20)
