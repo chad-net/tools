2c185fd0e06abf682acc2c8e70e29e8b69261d27
Chadnet <122728533+chad-net@users.noreply.github.com>
2023-05-26 23:17:23 +0000

    fix

--- wiki/site/update_index.py
+++ wiki/site/update_index.py
@@ -3,7 +3,7 @@ from wiki.constants import paths
 
 def update_index_page(total_link_count):
 	date = datetime.utcnow().strftime('%B %d, %Y, at %H:%M UTC')
-	replacement = f'  <p>Total links: <strong>{total_link_count}</strong>. Last updated on {date}.</p>\n'
+	replacement = f'  <p id="stats">Total links: <strong>{total_link_count}</strong>. Last updated on {date}.</p>\n'
 
 	with open(paths.WIKI_DIR / 'index.html', 'r', encoding='utf8') as file:
 		index_page_html = file.readlines()
