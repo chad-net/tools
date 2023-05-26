df019d914c9022ce301d9594565b92aca11914b4
Chadnet <122728533+chad-net@users.noreply.github.com>
2023-05-26 18:10:32 +0000

    Add files via upload

--- parser3000.sh
+++ parser3000.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# you can do the following: alias wiki='source ~/wiki_dir/tools/wiki.sh'
+# you can do the following: alias parser3000='source ~/wiki_dir/tools/parser3000.sh'
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 pushd "$SCRIPT_DIR" > /dev/null
-python -m wiki.main
+python -m parser3000.main
 popd > /dev/null
