8d2a2f5736d31eeb0251b2f7b1df1b99eac85c1c
Chadnet <122728533+chad-net@users.noreply.github.com>
2023-05-26 11:42:11 +0000

    v3.2.2

--- wiki/site/update_index.py
+++ wiki/site/update_index.py
@@ -1,15 +1,17 @@
 from datetime import datetime
-import re
 from wiki.constants import paths
 
 def update_index_page(total_link_count):
 	date = datetime.utcnow().strftime('%B %d, %Y, at %H:%M UTC')
-	replacement = f'<p>Total links: <strong>{total_link_count}</strong>. Last updated on {date}.</p>'
+	replacement = f'  <p>Total links: <strong>{total_link_count}</strong>. Last updated on {date}.</p>\n'
 
-	with open(paths.WIKI_DIR / 'index.html', 'r+', encoding='utf8') as file:
-		content = file.read()
-		content_new = re.sub(r'<p>Total links: <strong>.*<\/p>', replacement, content)
+	with open(paths.WIKI_DIR / 'index.html', 'r', encoding='utf8') as file:
+		index_page_html = file.readlines()
 
-		file.seek(0)
-		file.write(content_new)
-		file.truncate()
+	for line_number, line in enumerate(index_page_html):
+		if '<p id="stats">' in line:
+			index_page_html[line_number] = replacement
+			break
+
+	with open(paths.WIKI_DIR / 'index.html', 'w', encoding='utf8') as file:
+		file.writelines(index_page_html)
